f168fde568a6f5f2f5f89d5a38a6debfdf822105
ggustilo <beastifer@gmail.com>
2016-08-31 19:22:59 -0500

    Modularize

--- browser-scripts/maths.js
+++ browser-scripts/maths.js
@@ -1,16 +1,20 @@
 var $ = require('jquery');
 var math = require('mathjs');
 
-var subOperators = function(string){
-  if(string.includes("x")) {
-    var newString = string.replace("x", "*");
+module.exports = {
+  subOperators: function(string){
+    if(string.includes("x")) {
+      var newString = string.replace("x", "*");
+    }
+    if (string.includes("÷")) {
+      newString = string.replace("÷", "/");
+    }
+    return newString;
+  },
+  getResult: function(text) {
+    var firstPart = text.split('What is ')[1];
+    var secondPart = firstPart.split(' ?')[0];
+    var result = math.eval(subOperators(secondPart));
+    return result;
   }
-  if (string.includes("÷")) {
-    newString = string.replace("÷", "/");
-  }
-  return newString;
-}
-var text = $('.bot-question').text();
-var firstPart = text.split('What is ')[1];
-var secondPart = firstPart.split(' ?')[0];
-var result = math.eval(subOperators(secondPart));
+};
